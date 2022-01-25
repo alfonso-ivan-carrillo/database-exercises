330718471946dae00c0061b8f7b609090091566e
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-01-25 09:54:45 -0600

    completed db-tables exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,1 +1,10 @@
 USE codeup_test_db;
+CREATE TABLE IF NOT EXISTS albums (
+            id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+            artist CHAR(50) NOT NULL,
+            name CHAR(50) NOT NULL,
+            release_date DATE,
+            sales INT UNSIGNED NOT NULL,
+            genre CHAR(20),
+            PRIMARY KEY (id)
+);
