6949c09850234540a1e038c7b7319d741f24f0a8
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-01-27 16:18:48 -0600

    added notes to aliases lecture

--- aliases_lecture.sql
+++ aliases_lecture.sql
@@ -36,4 +36,12 @@ LIMIT 25;
 # Don't use aliases in WHERE clauses.
 # It is not permissible to refer to a column alias in a WHERE clause, because the column value might
 # not yet be determined when the WHERE clause is executed. This query will fail with an error:
+#
+# SELECT birth_date, CONCAT(first_name, ' ', last_name) AS full_name, gender
+# FROM employees
+# WHERE hire_date='1985-01-01'
+#   AND full_name='Arie Staelin';
 
+# WHERE is looking into the database, focused on a specific data in the database
+# GROUP BY or ORDER BY is using the results of the query to provide the information, thus Alias can
+# be used in the way because its using the results, not the actual data in the table/database
