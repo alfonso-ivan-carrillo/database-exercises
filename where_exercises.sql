c83b15e49addc888a80e3c6e8bc8f7ed1eb8cb03
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-01-27 09:28:22 -0600

    where statements - second part

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,13 +1,21 @@
 USE employees;
 
-SELECT first_name
+SELECT first_name, gender
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE first_name OR (first_name ='Irena' OR first_name = 'Vidya' OR first_name ='Maya')
+    AND gender = 'm';
 
-SELECT last_name
+SELECT *
 FROM employees
-WHERE last_name LIKE 'e%';
+WHERE last_name LIKE 'E%'
+    OR last_name Like '%E';
 
-SELECT last_name
+SELECT *
 FROM employees
-WHERE last_name LIKE '%q%';
+WHERE last_name LIKE 'E%E';
+# chain the e%e to find last names that begin and end with "e"
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%'
+    AND NOT last_name LIKE '%qu%';
